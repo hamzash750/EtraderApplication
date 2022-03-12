ea5f3597fb81d6679b4b83b264d25db0cc517819
hamzashafiq <hamzash75@gmail.com>
2022-03-12 18:11:02 +0500

    Master code

---
+++ eTraderApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTraderApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
